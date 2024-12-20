a0bb2c5b4e570c302058757578eec63964d4b2e6
mina-medhat-00 <mina.medhat.3141@gmail.com>
2024-12-20 16:05:53 +0200

    fix nodemon

--- server.js
+++ server.js
@@ -1,38 +1,26 @@
-/*---Setup Modules---*/
-// create empty JS object as endpoint for all routes
-let projectData = {};
-const bodyParser = require("body-parser");
-// import express module to run both routes and server
 const express = require("express");
-// start up instance of an app
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+let projectData = {};
 
-/*---Middleware---*/
-// configure express to use bodyParser as middleware
-// app.use() always used as middleware
 app.use(bodyParser.json());
-// we don't need to use the extended URL
 app.use(bodyParser.urlencoded({ extended: false }));
-// to tell node that main folder initialized is website
 app.use(express.static("website"));
-
-// importing CORS for cross origin allowance
-const cors = require("cors");
-// start using cors
 app.use(cors());
 
-app.post("/add", async function (req, res) {
+app.post("/add", async (req, res) => {
   const body = await req.body;
   projectData = body;
   res.send(projectData);
 });
 
-app.get("/all", async function (req, res) {
+app.get("/all", async (req, res) => {
   res.send(projectData);
 });
 
-// server listening when running
-app.listen(port, function () {
-  console.log(`server running...\nlistening in port ${port}`);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
